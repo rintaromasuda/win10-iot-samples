e1f7ab77ef6f45875107fe5ee7729bd877b88d56
Rintaro Masuda <rintaro.masuda@outlook.com>
2016-12-10 23:44:16 +0900

    Implemented PWM controller and the slider UI

--- LEDbyPWM/MainPage.xaml.cs
+++ LEDbyPWM/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices;
+using Windows.Devices.Gpio;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +14,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Pwm;
+using Windows.Devices.Pwm.Provider;
+using Windows.Devices.Lights;
+using Microsoft.IoT.Lightning.Providers;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +28,39 @@ namespace LEDbyPWM
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int OUTPUT_PIN = 25;
+        private PwmController _pwmController;
+        private PwmPin _outputPin;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            InitGPIO();
+        }
+
+        private async void InitGPIO()
+        {
+            if (LightningProvider.IsLightningEnabled)
+            {
+                LowLevelDevicesController.DefaultProvider = LightningProvider.GetAggregateProvider();
+                var pwmControllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
+                _pwmController = pwmControllers[1];
+
+                _outputPin = _pwmController.OpenPin(22);
+                _outputPin.SetActiveDutyCyclePercentage(0.5);
+                _outputPin.Start();
+            }
+        }
+
+        private void DutyCycleSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (_pwmController != null)
+            {
+                var slider = (Slider)sender;
+                var value = slider.Value;
+                _outputPin.SetActiveDutyCyclePercentage(value);
+            }
         }
     }
 }
