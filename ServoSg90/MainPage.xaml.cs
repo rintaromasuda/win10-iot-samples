d4063ca5af7c9ffbc99911f584e1d69691e7c27c
Rintaro Masuda <rintaro.masuda@outlook.com>
2016-12-12 23:14:31 +0900

    Implement the initial logic

--- ServoSg90/MainPage.xaml.cs
+++ ServoSg90/MainPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Microsoft.IoT.Lightning.Providers;
+using System;
+using Windows.Devices;
+using Windows.Devices.Pwm;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +14,66 @@ namespace ServoSg90
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int OUTPUT_PIN = 18;
+        private PwmController _pwmController;
+        private PwmPin _outputPin;
+
+        private double _pwmCycle = 20.0; // http://akizukidenshi.com/catalog/g/gM-08761/
+        private double _minPulse = 0.5;  // http://akizukidenshi.com/catalog/g/gM-08761/
+        private double _maxPulse = 2.4;  // http://akizukidenshi.com/catalog/g/gM-08761/
+
         public MainPage()
-        {
+        { 
             this.InitializeComponent();
+
+            InitGPIO();
+        }
+
+        private async void InitGPIO()
+        {
+            if (LightningProvider.IsLightningEnabled)
+            {
+                LowLevelDevicesController.DefaultProvider = LightningProvider.GetAggregateProvider();
+                var pwmControllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
+                _pwmController = pwmControllers[1];
+
+                _outputPin = _pwmController.OpenPin(18);
+                _outputPin.SetActiveDutyCyclePercentage(0.012);
+                _outputPin.Start();
+            }
+        }
+
+        private void Left90Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var percent = _minPulse / _pwmCycle;
+            _outputPin.SetActiveDutyCyclePercentage(percent);
+        }
+
+        private void Left45Buttono_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var length = (((_minPulse + _maxPulse) / 2) + _minPulse) / 2;
+            var percent = length / _pwmCycle;
+            _outputPin.SetActiveDutyCyclePercentage(percent);
+        }
+
+        private void CenterButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var length = (_minPulse + _maxPulse) / 2;
+            var percent = length / _pwmCycle;
+            _outputPin.SetActiveDutyCyclePercentage(percent);
+        }
+
+        private void Right45Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var length = (((_minPulse + _maxPulse) / 2) + _maxPulse) / 2;
+            var percent = length / _pwmCycle;
+            _outputPin.SetActiveDutyCyclePercentage(percent);
+        }
+
+        private void Right90Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var percent = _maxPulse / _pwmCycle;
+            _outputPin.SetActiveDutyCyclePercentage(percent);
         }
     }
 }
